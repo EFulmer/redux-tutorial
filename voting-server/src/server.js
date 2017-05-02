82c4bb670db0848076a5614816114792771fbaa1
Eric Fulmer <eric.r.fulmer@gmail.com>
2017-05-02 07:12:20 -0400

    last commit for server for now (from tutorial)

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,5 +1,22 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // store.subscribe takes a callback argument and runs it after every 
+  // update to the state
+  store.subscribe(
+    // io.emit sends the arguments to all connected clients
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  // when we receive a connection, execute the function 
+  io.on('connection', (socket) => {
+    // send the state over the socket
+    socket.emit('state', store.getState().toJS());
+    // set up a listener on this socket that waits for an action then 
+    // runs the function
+    socket.on('action', store.dispatch.bind(store));
+  });
+
 }
