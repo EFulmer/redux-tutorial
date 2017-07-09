1a7950cb9afe0949c2697bd9381b0032b05c8f10
Eric Fulmer <eric.r.fulmer@gmail.com>
2017-07-09 12:04:57 -0400

    finished react-redux on the client

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
-export default React.createClass({
+// pure component
+export const Voting = React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
       {this.props.winner ?
@@ -11,3 +15,13 @@ export default React.createClass({
       </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+// connected component
+export const VotingContainer = connect(mapStateToProps)(Voting);
